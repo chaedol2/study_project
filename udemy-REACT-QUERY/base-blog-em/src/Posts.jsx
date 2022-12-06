90ed09176c1aa0c3e731c8b4f41e127966ce1c7b
chaedol2 <chs891117@gmail.com>
2022-12-06 21:10:29 +0900

    feat: sec2: 14

--- udemy-REACT-QUERY/base-blog-em/src/Posts.jsx
+++ udemy-REACT-QUERY/base-blog-em/src/Posts.jsx
@@ -1,7 +1,7 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 import {PostDetail} from "./PostDetail";
-import {useQuery} from "react-query";
+import {useQuery, useQueryClient} from "react-query";
 
 const maxPostPage = 10;
 
@@ -16,8 +16,17 @@ export function Posts() {
     const [currentPage, setCurrentPage] = useState(1);
     const [selectedPost, setSelectedPost] = useState(null);
 
+    const queryClient = useQueryClient();
+
+    useEffect(() => {
+        if(currentPage < maxPostPage){
+            const nextPage = currentPage + 1;
+            queryClient.prefetchQuery(["posts", nextPage], ()=>fetchPosts(nextPage));
+        }
+    }, [currentPage, queryClient])
+
     // replace with useQuery
-    const {data, isError, error, isLoading} = useQuery(["posts", currentPage], ()=>fetchPosts(currentPage), {staleTime: 2000});
+    const {data, isError, error, isLoading} = useQuery(["posts", currentPage], ()=>fetchPosts(currentPage), {staleTime: 2000, keepPreviousData: true,}, );
     if (isLoading) return <h3>Loading...</h3>;
     if (isError) return (
         <>
